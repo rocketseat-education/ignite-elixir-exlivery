fe55371b7136360843f4c7bda2b76dbd37149274
Rafael Camarda <rf.camarda@gmail.com>
2021-02-08 16:02:55 -0300

    crete create_or_update order

--- lib/exlivery.ex
+++ lib/exlivery.ex
@@ -1,10 +1,14 @@
 defmodule Exlivery do
+  alias Exlivery.Orders.Agent, as: OrderAgent
+  alias Exlivery.Orders.CreateOrUpdate, as: CreateOrUpdateOrders
   alias Exlivery.Users.Agent, as: UserAgent
-  alias Exlivery.Users.CreateOrUpdate
+  alias Exlivery.Users.CreateOrUpdate, as: CreateOrUpdateUser
 
   def start_agents do
     UserAgent.start_link(%{})
+    OrderAgent.start_link(%{})
   end
 
-  defdelegate create_or_update_user(params), to: CreateOrUpdate, as: :call
+  defdelegate create_or_update_user(params), to: CreateOrUpdateUser, as: :call
+  defdelegate create_or_update_order(params), to: CreateOrUpdateOrders, as: :call
 end
