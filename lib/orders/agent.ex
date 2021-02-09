31f6a374fe2370283df6e7d7378e135291db7037
Rafael Camarda <rf.camarda@gmail.com>
2021-02-09 11:28:47 -0300

    add report creation

--- lib/orders/agent.ex
+++ lib/orders/agent.ex
@@ -16,6 +16,8 @@ defmodule Exlivery.Orders.Agent do
 
   def get(uuid), do: Agent.get(__MODULE__, &get_order(&1, uuid))
 
+  def list_all, do: Agent.get(__MODULE__, & &1)
+
   defp get_order(state, uuid) do
     case Map.get(state, uuid) do
       nil -> {:error, "Order not found"}
